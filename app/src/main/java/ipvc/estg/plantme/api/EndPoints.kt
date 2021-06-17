cb03fc081af728acee0f3341939e30bc1e987cfd
rodrigosa7 <digex2008@live.com.pt>
2021-06-17 16:38:34 +0100

    Fix Import Files

--- app/src/main/java/ipvc/estg/plantme/api/EndPoints.kt
+++ app/src/main/java/ipvc/estg/plantme/api/EndPoints.kt
@@ -1,10 +1,7 @@
 package ipvc.estg.plantme.api
 
 import ipvc.estg.plantme.api.entidades.User
-import ipvc.estg.plantme.api.respostas.RespostaEventosPlantacao
-import ipvc.estg.plantme.api.respostas.RespostaPlantacoes
-import ipvc.estg.plantme.api.respostas.RespostaProduto
-import ipvc.estg.plantme.api.respostas.RespostaSugestao
+import ipvc.estg.plantme.api.respostas.*
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -18,23 +15,21 @@ interface EndPoints {
     ): Call<User>
 
     @GET("/plantacoes/getAll/{email}")
-    fun getAllPlantacoes(@Path("email") email: String?) : Call<RespostaPlantacoes>
+    fun getAllPlantacoes(@Path("email") email: String?): Call<RespostaPlantacoes>
 
     @GET("/produtos/getById/{id}")
-    fun getProdutoId(@Path("id") id: Int?) : Call<RespostaProduto>
+    fun getProdutoId(@Path("id") id: Int?): Call<RespostaProduto>
 
     @GET("/eventos/getLatest/{id}")
-    fun getEventosRecentes(@Path("id") id: Int?) : Call<RespostaEventosPlantacao>
+    fun getEventosRecentes(@Path("id") id: Int?): Call<RespostaEventosPlantacao>
 
     @GET("/sugestoes/getEpocaAtual")
     fun getEpocaAtual(): Call<RespostaSugestao>
-<<<<<<< Updated upstream
-=======
+
 
     @GET("/vendas/getMesAno/{ano}/{mes}")
     fun getVendasMesAno(@Path("ano") ano: Int?, @Path("mes") mes: Int?): Call<RespostaVenda>
 
     @GET("/vendas/getAll")
     fun getVendasAll(): Call<RespostaVenda>
->>>>>>> Stashed changes
 }
--- app/src/main/java/ipvc/estg/plantme/ui/home/HomeFragment.kt
+++ app/src/main/java/ipvc/estg/plantme/ui/home/HomeFragment.kt
@@ -1,36 +1,56 @@
 package ipvc.estg.plantme.ui.home
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import ipvc.estg.plantme.R
+import ipvc.estg.plantme.api.EndPoints
+import ipvc.estg.plantme.api.ServiceBuilder
+import ipvc.estg.plantme.api.entidades.Plantacao
+import ipvc.estg.plantme.api.respostas.RespostaPlantacoes
+import ipvc.estg.plantme.api.respostas.RespostaVenda
+import ipvc.estg.plantme.ui.plantacoes.DetalhesPlantacao
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var email: String
+
+    private lateinit var nomePlantacao1 :TextView
+    private lateinit var nomePlantacao2: TextView
+    private lateinit var imagePlantacao1: ImageView
+    private lateinit var imagePlantacao2: ImageView
+
+    private lateinit var plantacao1: Plantacao
+    private lateinit var plantacao2: Plantacao
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-<<<<<<< Updated upstream
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
-    }
-}
-=======
+
         sharedPreferences = this.requireActivity().getSharedPreferences(getString(R.string.plantme), Context.MODE_PRIVATE)
         email = sharedPreferences.getString(getString(R.string.email_sp), "").toString()
         nomePlantacao1 = root.findViewById(R.id.plantacao1)
@@ -43,7 +63,7 @@ class HomeFragment : Fragment() {
             intent.putExtra("plantacao", plantacao1)
             startActivity(intent)
         }
-        
+
         imagePlantacao2.setOnClickListener {
             val intent = Intent(this.requireContext(), DetalhesPlantacao::class.java)
             intent.putExtra("plantacao", plantacao2)
@@ -123,27 +143,30 @@ class HomeFragment : Fragment() {
 
                         } else {
                             Toast.makeText(
-                                    this@HomeFragment.requireContext(),
-                                    getString(R.string.erro_api),
-                                    Toast.LENGTH_LONG
+                                this@HomeFragment.requireContext(),
+                                getString(R.string.erro_api),
+                                Toast.LENGTH_LONG
                             ).show()
                         }
                     }
                     else {
                         Toast.makeText(
-                                this@HomeFragment.requireContext(),
-                                getString(R.string.erro_pedido),
-                                Toast.LENGTH_LONG
+                            this@HomeFragment.requireContext(),
+                            getString(R.string.erro_pedido),
+                            Toast.LENGTH_LONG
                         ).show()
                     }
                 }
                 override fun onFailure(call: Call<RespostaPlantacoes>, t: Throwable) {
-                    Log.i("PLANTACOES",  t.message.toString())
+
                     Toast.makeText(this@HomeFragment.requireContext(), t.message, Toast.LENGTH_SHORT).show()
                 }
             })
         }
         return root
+
     }
 }
->>>>>>> Stashed changes
+
+
+
